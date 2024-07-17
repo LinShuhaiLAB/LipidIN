778044a130110e44265f708c1e6b8eb9dd3dc59c
LinShuhaiLAB <154107118+LinShuhaiLAB@users.noreply.github.com>
2024-07-17 15:43:48 +0800

    Update fastMatch.cpp

--- LipidIN/MS-DIAL published library/fastMatch.cpp
+++ LipidIN/MS-DIAL published library/fastMatch.cpp
@@ -20,7 +20,7 @@ fastMatch::fastMatch(std::vector<ms1>& ms1_vecotr, std::vector<ms2>& ms2_vector,
 
 void fastMatch::compare()
 {
-    // 输出的csv文件
+
     ofstream dataFile(this->m_output_path);
     dataFile << "index"
              << ","
@@ -35,7 +35,7 @@ void fastMatch::compare()
              << "score1"
              << ","
              << "score2" << endl;
-    // 进行查找
+
     for (auto itr = this->m_ms1_vecotr.begin(); itr != this->m_ms1_vecotr.end(); itr++) {
         itr->compare(this->m_library_min_mz, this->m_library_max_mz, this->m_ms2_vector, dataFile);
     }
@@ -82,32 +82,32 @@ ms1::ms1(double precursor_mz, double precursor_intensity, double rt, ms2 ms2, st
 
 void ms1::compare(std::vector<double>& min_mz, std::vector<double>& max_mz, std::vector<ms2>& ms2_vector, std::ofstream& data_file)
 {
-    // 二分查找,min_mz需要已经排序好
+
     int left = 0;
     int right = min_mz.size() - 1;
 
     while (left <= right) {
         int mid = (left + right) / 2;
         if (min_mz.at(mid) <= this->m_precursor_mz && this->m_precursor_mz <= max_mz.at(mid)) {
-            // 比较两个二级
+
             pair<double, double> compare_ms2_res = this->m_ms2.compare(ms2_vector.at(mid));
             if (compare_ms2_res.first != 0 && compare_ms2_res.second != 0) {
-                // 输出信息
+
                 data_file << this->m_index << "," << this->m_precursor_mz << "," << this->m_rt << "," << this->m_precursor_intensity << "," << ms2_vector.at(mid).GetInfo() << "," << compare_ms2_res.first << "," << compare_ms2_res.second << endl;
             }
-            // 向左和右继续寻找
+
             int left_t = mid - 1;
             int right_t = mid + 1;
 
             while (left_t >= 0) {
                 if (min_mz.at(left_t) <= this->m_precursor_mz && this->m_precursor_mz <= max_mz.at(left_t)) {
-                    // 比较两个二级
+
                     compare_ms2_res = this->m_ms2.compare(ms2_vector.at(left_t));
                     if (compare_ms2_res.first != 0 && compare_ms2_res.second != 0) {
-                        // 输出信息
+
                         data_file << this->m_index << "," << this->m_precursor_mz << "," << this->m_rt << "," << this->m_precursor_intensity << "," << ms2_vector.at(left_t).GetInfo() << "," << compare_ms2_res.first << "," << compare_ms2_res.second << endl;
                     }
-                    // 向左继续找
+
                     left_t--;
                 } else {
                     break;
@@ -115,19 +115,19 @@ void ms1::compare(std::vector<double>& min_mz, std::vector<double>& max_mz, std:
             }
             while (right_t <= int(min_mz.size() - 1)) {
                 if (min_mz.at(right_t) <= this->m_precursor_mz && this->m_precursor_mz <= max_mz.at(right_t)) {
-                    // 比较两个二级
+
                     compare_ms2_res = this->m_ms2.compare(ms2_vector.at(right_t));
                     if (compare_ms2_res.first != 0 && compare_ms2_res.second != 0) {
-                        // 输出信息
+
                         data_file << this->m_index << "," << this->m_precursor_mz << "," << this->m_rt << "," << this->m_precursor_intensity << "," << ms2_vector.at(right_t).GetInfo() << "," << compare_ms2_res.first << "," << compare_ms2_res.second << endl;
                     }
-                    // 向左继续找
+
                     right_t++;
                 } else {
                     break;
                 }
             }
-            break; // 找完左边和右边的，就说明ms1_itr的元素已经找完了，跳出这个while
+            break; 
         } else if (this->m_precursor_mz <= min_mz.at(mid)) {
             right = mid - 1;
         } else if (this->m_precursor_mz >= max_mz.at(mid)) {
@@ -169,7 +169,7 @@ ms2::ms2(std::vector<double> mz, std::vector<double> intensity, std::vector<doub
     this->m_info = move(info);
 
     if (count_total_intensity) {
-        // 计算总强度
+
         double total_intensity = 0;
         for (double num : m_intensity) {
             total_intensity += num;
@@ -182,15 +182,14 @@ ms2::ms2(std::vector<double> mz, std::vector<double> intensity, std::vector<doub
 
 std::pair<double, double> const ms2::compare(ms2& library_ms2)
 {
-    // 二分查找,library_ms2的min_mz和max_mz需要已经排序好
+
     vector<double>& library_min_mz = library_ms2.GetMinMz();
     vector<double>& library_max_mz = library_ms2.GetMaxMz();
     vector<double>& library_mz = library_ms2.GetMz();
 
-    // 用于去重
     unordered_map<double, int> m_hash_map;
 
-    // first存储score1，second存储score2
+
     pair<double, double> res;
 
     for (auto itr = this->m_mz.begin(); itr != this->m_mz.end(); itr++) {
@@ -200,15 +199,15 @@ std::pair<double, double> const ms2::compare(ms2& library_ms2)
         while (left <= right) {
             int mid = (left + right) / 2;
             if (library_min_mz.at(mid) <= *(itr) && *(itr) <= library_max_mz.at(mid)) {
-                auto find_itr = m_hash_map.find(library_mz.at(mid)); // 寻找这个理论库的这个碎片是否被匹配过
-                // 如果未被匹配过,将理论库的mz作为键，样本匹配上的碎片的位置存入哈希表
+                auto find_itr = m_hash_map.find(library_mz.at(mid)); 
+
                 if (find_itr == m_hash_map.end()) {
                     m_hash_map[library_mz.at(mid)] = itr - this->m_mz.begin();
                 } else {
-                    // 计算上一个匹配的diff和新匹配上的diff
+
                     double origin_dif = abs(find_itr->first - this->m_mz.at(find_itr->second));
                     double new_diff = abs(find_itr->first - (*itr));
-                    // 如果新的diff比上一次匹配的diff小，则更新哈希表对应的值
+
                     if (new_diff <= origin_dif) {
                         m_hash_map[find_itr->first] = itr - this->m_mz.begin();
                     }
@@ -225,13 +224,13 @@ std::pair<double, double> const ms2::compare(ms2& library_ms2)
     double score1 = 0;
     double score2 = 0;
 
-    // 计算score1
+
     for (auto itr = m_hash_map.begin(); itr != m_hash_map.end(); itr++) {
         score2 += this->m_intensity.at(itr->second);
     }
     score2 = score2 / this->m_total_intensity;
 
-    // 计算score2
+
     score1 = double(m_hash_map.size()) / library_mz.size();
 
     res.first = score1;
@@ -264,7 +263,7 @@ string ms2::GetInfo()
     return this->m_info;
 }
 
-// [[Rcpp::export]]
+
 void SuperFastCompare(Rcpp::List& sample_List, std::vector<double>& library_mz, double ppm, Rcpp::List& library_list, std::string output_path)
 {
     vector<ms1> ms1_vector;
@@ -272,33 +271,31 @@ void SuperFastCompare(Rcpp::List& sample_List, std::vector<double>& library_mz, 
     clock_t start, end;
 
     start = clock();
-    // 载入一级向量
+
     for (int i = 0; i < sample_List.size(); i++) {
         Rcpp::List single_List = sample_List.at(i);
         double precursor_mz = single_List["PrecursorMZ"];
         double precursor_intensity = single_List["PrecursorIntensity"];
         double rt = single_List["rt"];
         string index = single_List["num"];
-        // 获取二级那个数据框
+
         Rcpp::List ms2_List = single_List["MS2mz"];
         vector<double> ms2_mz = ms2_List["da.temp.mz"];
         vector<double> ms2_intensity = ms2_List["da.temp.intensity"];
 
         vector<double> null_vector = {};
 
-        // 二级
+
         ms2 this_ms2 = ms2(move(ms2_mz), move(ms2_intensity), null_vector, null_vector, "", true);
 
-        // 加入ms1_vector中
+
         ms1_vector.emplace_back(ms1(precursor_mz, precursor_intensity, rt, move(this_ms2), move(index)));
     }
 
-    // 载入二级向量
     for (int i = 0; i < library_list.size(); i++) {
         Rcpp::List single_List = library_list.at(i);
         string info = single_List["inf"];
 
-        // 获取二级那个数据框
         Rcpp::List ms2_List = single_List["ms2.spe"];
 
         vector<double> ms2_mz = ms2_List["mz"];
@@ -306,7 +303,7 @@ void SuperFastCompare(Rcpp::List& sample_List, std::vector<double>& library_mz, 
         vector<double> ms2_min_mz = ms2_List["down"];
         vector<double> ms2_max_mz = ms2_List["up"];
 
-        // 加入ms2_vector中
+
         ms2_vector.emplace_back(ms2(move(ms2_mz), move(ms2_intensity), move(ms2_min_mz), move(ms2_max_mz), info, false));
     }
     end = clock();
