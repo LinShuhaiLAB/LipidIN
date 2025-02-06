338603a4b0a57f19fea61f5026255c4efdaa2604
Hao Xu <148458997+HaroldHaoXu@users.noreply.github.com>
2025-02-06 16:28:51 +0800

    Update code for launch UI.R

--- LipidIN GUI/code for launch UI.R
+++ LipidIN GUI/code for launch UI.R
@@ -13,7 +13,7 @@ for (pkg in packages) {
 library(devtools)
 
 install.packages(
-  pkgs = 'D:/bio_inf/LipidIN-main/LipidIN GUI/LipidIN_2.0.0.1.tar.gz',
+  pkgs = 'D:/bio_inf/LipidIN-main/LipidIN GUI/LipidIN_2.0.0.2.tar.gz',
   # Note that here you need to write the full address 
   # of LipidIN_2.0.0.1.tar.gz, the above is only an example, 
   # please modify the address according to the actual situation.
@@ -25,7 +25,11 @@ install.packages(
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
-BiocManager::install("xcms")
-BiocManager::install("CAMERA")
+if (!require("xcms", quietly = TRUE))
+  BiocManager::install("xcms")
+
+if (!require("CAMERA", quietly = TRUE))
+  BiocManager::install("CAMERA")
+
 library(LipidIN)
 run_LipidIN()
